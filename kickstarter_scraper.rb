6d3c3621f7980cf536d79832268565b01636dbac
Naya  Willis <greedybrain18@gmail.com>
2019-10-17 15:13:18 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,8 +8,8 @@ def create_project_hash
   # title: project.css("h2.bbcard_name strong a").text
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
   # description: project.css("p.bbcard_blurb").text
-  # project.css('span.location-name')
-  # project.css('ul.project-stats li.first.funded')
+  # project.css('span.location-name').text
+  # project.css('ul.project-stats li.first.funded').text
   binding.pry
 end
 
