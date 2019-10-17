1bd4b88020f9a75802255b5e4ad49a76f24cff97
Naya  Willis <greedybrain18@gmail.com>
2019-10-17 15:20:04 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,7 +1,6 @@
 require "nokogiri"
 require "pry"
 
-  # title: project.css("h2.bbcard_name strong a").text
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
   # description: project.css("p.bbcard_blurb").text
   # project.css('ul.project-meta span.location-name').text
@@ -14,7 +13,8 @@ def create_project_hash
   projects = {}
   
   kickstarter.css("li.project.grid_4").each do |project|
-    projects[project] = {}
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
   end
   projects
   
