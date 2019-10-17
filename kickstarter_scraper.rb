f14e2012ef1b43b57c9524b70bf47500f4b6333e
Naya  Willis <greedybrain18@gmail.com>
2019-10-17 15:19:04 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,7 +1,6 @@
 require "nokogiri"
 require "pry"
 
-# projects: kickstarter.css("li.project.grid_4")
   # title: project.css("h2.bbcard_name strong a").text
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
   # description: project.css("p.bbcard_blurb").text
@@ -12,7 +11,14 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html) 
   
-  binding.pry
+  projects = {}
+  
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
+  projects
+  
+  # binding.pry
 end
 
 create_project_hash
